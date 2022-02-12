84111cd46fd50ea71b05192bdeeaa782e7743820
Debajyoti <debajyotisaha14@gmail.com>
2022-02-12 21:55:49 +0530

    Filter scrolll & Icon

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -193,18 +193,6 @@ const Navbar = (props) => {
                     }}
                   >
                     <span className="text-skin-darkBlue flex items-center">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-5 w-5 mr-2 text-black"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
-                          clipRule="evenodd"
-                        />
-                      </svg>{" "}
                       User Profile
                     </span>
                   </MenuItem>
@@ -220,18 +208,6 @@ const Navbar = (props) => {
                     }}
                   >
                     <span className="text-skin-darkBlue flex items-center ">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-5 w-5 mr-2 text-red-500"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
                       Favourite
                     </span>
                   </MenuItem>
@@ -247,18 +223,6 @@ const Navbar = (props) => {
                     }}
                   >
                     <span className="text-skin-darkBlue flex items-center">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-5 w-5 mr-2"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
                       Your Orders
                     </span>
                   </MenuItem>
@@ -272,18 +236,6 @@ const Navbar = (props) => {
                   }}
                 >
                   <span className="text-skin-darkBlue flex items-center">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-5 w-5 mr-2"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
                     Logout
                   </span>
                 </MenuItem>
